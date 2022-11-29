d66ec73337487c874043555204e49725abfb79c2
marloxxx <66661134+marloxxx@users.noreply.github.com>
2022-11-29 10:22:07 +0700

    update

--- UTS/SiBotoBangun/app/src/main/java/com/example/sibotobangun/models/User.kt
+++
@@ -1,9 +0,0 @@
-package com.example.sibotobangun.models
-
-data class User(
-    val nik: String,
-    val fullname: String,
-    val phone: String,
-    val email: String,
-    val password: String
-)
--- UTS/SiBotoBangun/app/src/main/java/com/example/sibotobangun/utils/FragmentNavigation.kt
+++
@@ -1,7 +0,0 @@
-package com.example.sibotobangun.utils
-
-import androidx.fragment.app.Fragment
-
-interface FragmentNavigation {
-    fun navigateTo(fragment: Fragment, addToBackstack: Boolean)
-}
