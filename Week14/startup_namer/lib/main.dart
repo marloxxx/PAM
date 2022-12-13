f6914eaabfc4650e56f8e049bcf18de62d5e41b6
marloxxx <66661134+marloxxx@users.noreply.github.com>
2022-12-13 11:21:39 +0700

    update

--- Week14/startup_namer/lib/main.dart
+++ Week14/startup_namer/lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'random_words.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,6 +20,39 @@ class MyApp extends StatelessWidget {
           child: RandomWords(),
         ),
       ),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({super.key});
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[]; // NEW
+  final _biggerFont = const TextStyle(fontSize: 18); // NEW
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return const Divider();
+
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return ListTile(
+          title: Text(
+            _suggestions[index].asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+      },
     );
   }
 }
